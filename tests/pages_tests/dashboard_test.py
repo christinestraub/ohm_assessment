b949692c484ddd609973bf131d8d420094f56c89
christinestraub <christinemstraub@gmail.com>
2020-04-24 07:02:20 -0700

    [test]: Added tests for user model and dashboard.

--- tests/pages_tests/dashboard_test.py
+++ tests/pages_tests/dashboard_test.py
@@ -4,7 +4,14 @@ from tests import OhmTestCase
 
 
 class DashboardTest(OhmTestCase):
-    def test_get(self):
+    @staticmethod
+    def test_dashboard():
         with app.test_client() as c:
             response = c.get('/dashboard')
             assert "Ready to begin assessment" in response.data
+
+    @staticmethod
+    def test_community_response():
+        with app.test_client() as c:
+            response = c.get('/community')
+            assert "Recent Signed Users" in response.data
